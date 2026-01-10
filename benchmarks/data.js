59a803a722f1c1dae655c31629b6f1cd92d0bb5d
github-action-benchmark <github@users.noreply.github.com>
2026-01-10 20:37:34 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for ea4ca3d9508982500044710112d3d34f4648cfd8

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768077426918,
+  "lastUpdate": 1768077454983,
   "repoUrl": "https://github.com/baxterlabsai/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -23,6 +23,37 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/baxterlabsai/n8n-mcp/commit/d7e87d85b76c31f78af115853e62203135fc1daf"
         },
         "date": 1768077425997,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "george@baxterlabs.ai",
+            "name": "baxterlabsai",
+            "username": "baxterlabsai"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ea4ca3d9508982500044710112d3d34f4648cfd8",
+          "message": "Delete weekly-sync.yml",
+          "timestamp": "2026-01-10T12:35:27-08:00",
+          "tree_id": "6d5d6cb7fe9f62060a0e97d70559c4cc97e690ec",
+          "url": "https://github.com/baxterlabsai/n8n-mcp/commit/ea4ca3d9508982500044710112d3d34f4648cfd8"
+        },
+        "date": 1768077454253,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
